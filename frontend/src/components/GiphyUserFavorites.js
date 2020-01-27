6d8fbf1912510436ccb7a1d7c62f949345b13130
Andrew Yeager <andrew.yeager@q2ebanking.com>
2020-01-26 22:36:40 -0600

    still trying to figure out React modals for favorite categorization.

--- frontend/src/components/GiphyUserFavorites.js
+++ frontend/src/components/GiphyUserFavorites.js
@@ -1,6 +1,60 @@
 import React, {Component} from "react";
+import ReactDOM from "react-dom";
 
 import Gallery from "react-grid-gallery";
+import Popup from "reactjs-popup";
+
+
+function Modal(){
+    return (
+        <Popup
+            trigger={<button className="btn" id="open_modal"> Open Modal </button>}
+            modal
+            closeOnDocumentClick
+        >
+            <span> Modal content </span>
+        </Popup>
+    );
+}
+
+class ControlledPopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: true };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({ open: true });
+  }
+  closeModal() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <Popup
+          open={this.state.open}
+          closeOnDocumentClick
+          onClose={this.closeModal}
+        >
+          <div className="modal">
+            <a className="close" onClick={this.closeModal}>
+              &times;
+            </a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni
+            omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate
+            ea, accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam
+            doloribus. Odit, aut.
+            </p>
+          </div>
+        </Popup>
+      </div>
+    );
+  }
+}
+
 
 
 class GiphyUserFavorites extends Component {
@@ -8,16 +62,25 @@ class GiphyUserFavorites extends Component {
         super(props);
         this.state = {
             favorites_data: [],
-            images: []
+            images: [],
+            open_modal: true,
         }
     }
 
     componentWillReceiveProps(props) {
-        var images = props.favorites_data.map(data => {return data.image_data})
+        var images = props.favorites_data.map(data => {
+            return data.image_data
+        });
         this.setState({
             favorites_data: props.favorites_data,
             images: images
         });
+    }
+
+    renderPopup(index) {
+        alert('here is where we will allow things like categorization and deletion of the favorite row.  ' +
+            'Currently trying to figure out modals');
+        // ReactDOM.render(<ControlledPopup/>, document.getElementById('modal'))
     }
 
     render() {
@@ -25,20 +88,20 @@ class GiphyUserFavorites extends Component {
             <div>
                 <div className="row col-sm-12">
                     <div style={{
-                            minHeight: "1px",
-                            maxHeight: "540px",
-                            width: "100%",
-                            border: "1px solid #ddd",
-                            overflow: "auto",}}
+                        minHeight: "1px",
+                        maxHeight: "540px",
+                        width: "100%",
+                        border: "1px solid #ddd",
+                        overflow: "auto",
+                    }}
                     >
                         <Gallery
                             images={this.state.images}
-                            enableImageSelection={false}
-                            enableLightbox={true}
-                            currentImageWillChange={this.onCurrentImageChange}
-                            showLightboxThumbnails={true}
-
+                            enableImageSelection={true}
+                            enableLightbox={false}
+                            // onSelectImage={this.renderPopup}
                         />
+
                     </div>
                 </div>
             </div>
